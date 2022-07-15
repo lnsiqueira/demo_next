9e4772a2f3c47e069727749c3e87979e22c65653
luciano <lnsiqueira@gmail.com>
2022-07-15 16:05:23 +0200

    new apoi

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import Script from 'next/script';
 import { useState } from 'react';
-
 
 function Home() {
     return (
         <div>
+
+
             <h1>Home new</h1>
 
             <Link href='/sobre'>
@@ -12,6 +14,9 @@ function Home() {
             </Link>
 
             <Contador />
+
+            <Script async data-uid="3a8681ecee" src="https://awesome-producer-2056.ck.page/3a8681ecee/index.js" />
+
         </div>
 
 
